382655b45c131b31f0fb1c50ca5213b90c79c684
Alexey Chuprin <a.chuprin@capjack.ru>
2018-07-13 00:01:12 +0300

    Update build configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,9 @@
+import org.jetbrains.kotlin.gradle.plugin.KotlinPluginWrapper
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("kotlin") version "1.2.41" apply false
-	id("ru.capjack.degos.publish") version "1.5.0" apply false
+	id("kotlin") version "1.2.51" apply false
+	id("ru.capjack.degos.publish") version "1.7.0" apply false
 	id("nebula.release") version "6.0.0"
 }
 
@@ -10,7 +11,7 @@ subprojects {
 	group = "ru.capjack.biser"
 	repositories.maven("http://artifactory.capjack.ru/public")
 	
-	plugins.withId("java") {
+	plugins.withType<KotlinPluginWrapper> {
 		configure<JavaPluginConvention> {
 			sourceCompatibility = JavaVersion.VERSION_1_8
 		}
